4d4b7a2d3a7976dd4f748e512848aae00314eb83
jmeziere <jmeziere@gmail.com>
2024-08-16 23:18:25 -0600

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,8 @@ makedocs(
         prettyurls = get(ENV, "CI", nothing) == "true"
     ),
     pages = [
-        "Main"=>"index.md",
+        "BCDI"=>"index.md",
+        "BcdiMeso"=>"main.md",
         "Usage"=>"use.md"
     ]
 )
